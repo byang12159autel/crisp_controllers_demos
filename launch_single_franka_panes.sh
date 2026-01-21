1ed539e012b8ad2e2940d972b6ff109a0514096c
Ben <benjaminyang@autel.com>
2026-01-21 18:45:14 -0500

    Single franka launch script

--- launch_single_franka_panes.sh
+++ launch_single_franka_panes.sh
@@ -69,12 +69,6 @@ if [ "$USE_SIM" = "sim" ]; then
     # Sim Version
     echo "Launching in SIMULATION mode..."
     tmux send-keys -t "$SESSION_NAME:0.0" "ROBOT_IP=$ROBOT_IP FRANKA_FAKE_HARDWARE=true RMW=cyclone ROS_NETWORK_INTERFACE=$ROS_NETWORK_INTERFACE docker compose up launch_franka" C-m
-    # if [ "$DISABLE_RVIZ" = "no_rviz" ]; then
-    #     echo "Disabling RViz for dual franka launch..."
-    #     tmux send-keys -t "$SESSION_NAME:0.0" "LEFT_ROBOT_IP=172.16.1.2 RIGHT_ROBOT_IP=172.16.0.2 DUAL_FRANKA_USE_RVIZ=false DUAL_FRANKA_ENABLE_MUJOCO_GUI=true FRANKA_FAKE_HARDWARE=true RMW=cyclone ROS_NETWORK_INTERFACE=$ROS_NETWORK_INTERFACE docker compose up launch_dual_franka" C-m
-    # else
-    #     tmux send-keys -t "$SESSION_NAME:0.0" "LEFT_ROBOT_IP=172.16.1.2 RIGHT_ROBOT_IP=172.16.0.2 FRANKA_FAKE_HARDWARE=true RMW=cyclone ROS_NETWORK_INTERFACE=$ROS_NETWORK_INTERFACE docker compose up launch_dual_franka" C-m
-    # fi
 
 else
     # Hardware Version
